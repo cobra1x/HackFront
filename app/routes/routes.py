257d2a1130e99bc0fe0ec79882558d04e7a71f1d
cobra1x <chiranjibroy655@gmail.com>
2026-09-14 20:38:24 +0530

    first commit

---
+++ app/routes/routes.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter,UploadFile, File
+from typing import List
+from app.core.llm import client
+from app.templates.system_prompt import system_prompt
+
+router=APIRouter()
+contents = []
+
+
+
+@router.get("/")
+def home():
+    return {"message":"hello from server"}
+
+@router.post("/document/upload")
+def upload(files:list[UploadFile] = File(...)):
+    for file in files:
+        with open(f"docs/{file.filename}", "wb") as f:
+            f.write(file.file.read())
+
+        gemini_files=client.files.upload(file=f"docs/{file.filename}")
+        contents.append(gemini_files)
+
+    return {"message": "Files uploaded successfully"}
+    # name=None
+
+@router.get("/delay_average")
+def delay_average():
+    res=client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[system_prompt]+contents
+    )
+
+    return res.text
---
+++ app/templates/system_prompt.py
@@ -0,0 +1,17 @@
+system_prompt="""
+    Analyze all the provided invoice and payment receipt documents.
+
+    For each invoice:
+    1. Find the invoice date and invoice ID.
+    2. Find the corresponding payment receipt.
+    3. Find the payment date.
+    4. Calculate payment delay in days.
+    5. If payment was not received, discard that invoice from the calculation.
+
+    Finally calculate the average payment delay using only invoices
+    for which payment was received.
+
+    Return ONLY the average payment delay as an integer.
+    Do not return JSON, markdown, explanation, or decimal values.
+    **IMPORTANT:recheck the calculations again so that no scope of error**
+    """
